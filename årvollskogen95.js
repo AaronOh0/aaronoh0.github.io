a6a7986bf13f5df996ef4dcab18d4334c000e73e
Åron <167232717+AaronOh0@users.noreply.github.com>
2024-08-09 14:54:14 +0200

    Add files via upload

--- årvollskogen95.js
+++ årvollskogen95.js
@@ -8,24 +8,29 @@ let currentSlide = 0;
 
 const slides = {
   0: {
-    src: "bilderEier/Transfer_4/ewewewewq.jpg",
-    caption: "Bilde over Årevollskogen 91",
-    slideNumber: "1/4",
+    src: "bilderEier/Transfer_2/Arvollskogen 95 (1 of 16).jpg",
+    caption: "Inngang til Årvollskogen 95",
+    slideNumber: "1/5",
   },
   1: {
-    src: "bilderEier/Transfer_4/16802732.jpg",
+    src: "bilderEier/Transfer_2/Arvollskogen 95 (3 of 16).jpg",
     caption: "Inngang til Årevollskogen 91",
-    slideNumber: "2/4",
+    slideNumber: "2/5",
   },
   2: {
-    src: "bilderEier/Transfer_4/16802730.jpg",
+    src: "bilderEier/Transfer_2/Arvollskogen 95 (4 of 16).jpg",
     caption: "Caption",
-    slideNumber: "3/4",
+    slideNumber: "3/5",
   },
   3: {
-    src: "bilderEier/Transfer_4/16802725.jpg",
+    src: "bilderEier/Transfer_2/Arvollskogen 95 (7 of 16).jpg",
     caption: "Skriv inn noe",
-    slideNumber: "4/4",
+    slideNumber: "4/5",
+  },
+  4: {
+    src: "bilderEier/Transfer_2/Arvollskogen 95 (16 of 16).jpg",
+    caption: "Noe",
+    slideNumber: "5/5",
   },
 };
 
@@ -44,7 +49,7 @@ function updateSlide() {
     document.querySelector(".prev").style.display = "none";
   } else if (currentSlide == 1) {
     document.querySelector(".prev").style.display = "block";
-  } else if (currentSlide == 3) {
+  } else if (currentSlide == 4) {
     document.querySelector(".next").style.display = "none";
   } else {
     document.querySelector(".next").style.display = "block";
